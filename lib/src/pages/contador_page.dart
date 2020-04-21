698f60434e203c5b076fc8e6aaa2924c4a1571e6
Antonio Ayala <antonio.ayala.a@gmail.com>
2020-04-20 21:29:23 -0400

    App Contador

---
+++ lib/src/pages/contador_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+
+class ContadorPage extends StatefulWidget {
+
+  @override
+  createState() => _ContadorPageState();
+
+}
+
+
+
+
+class _ContadorPageState extends State <ContadorPage> {
+
+  final TextStyle _estiloTexto = new TextStyle(fontSize: 30);
+  int _conteo = 10;
+
+ @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Titulo'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget> [
+            Text('Numero de clicks', style: _estiloTexto),
+            Text( '$_conteo' ,style: _estiloTexto),
+          ],
+        ),
+      ),
+      floatingActionButton: _crearBotones(),
+    );
+  }
+
+  Widget _crearBotones () {
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        FloatingActionButton( child: Icon(Icons.exposure_zero), onPressed: _reset),
+        SizedBox(width: 5.0),
+        FloatingActionButton( child: Icon(Icons.remove), onPressed: _sustraer),
+        SizedBox(width: 5.0),
+        FloatingActionButton( child: Icon(Icons.add), onPressed: _agregar),
+      ],
+    );
+  }
+
+  void _agregar () {
+    setState(() => _conteo++);
+  }
+
+  void _sustraer () {
+    setState(() => _conteo--);  
+  }
+
+  void _reset () {
+    setState(() => _conteo = 0);
+
+  }
+
+}
